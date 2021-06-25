68a34e5592d8eec699e9d6490b7c7aea4dd82222
Pedro Cisdeli <pedrohcsideli@usp.br>
2021-06-25 00:00:40 -0300

    Local credentials login -unfinished-

--- frontend/src/components/authentication/login.js
+++ frontend/src/components/authentication/login.js
@@ -1,28 +1,83 @@
-import React from "react";
+import React, {
+  Component
+} from 'react';
 
 import '../../css/login_signup.css';
 
+import mockCreds from './mockCredentials'
 
-const Login = () => (
-    <div class="container py-4">
-        <main class="form-signin text-center">
-            <form>
-                <h1 class="h3 mb-3 fw-normal">Login</h1>
-                <div class="form-floating">
-                    <input type="email" class="form-control" id="Name" placeholder="name@example.com" />
-                    <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="RePassword" placeholder="Password" />
-                    <label for="floatingPassword">Password</label>
-                </div>
-                <div class="mb-3">
-                    <a href="sign_up.html">Not registered yet? Sign up!</a>
-                </div>
-                <button class="w-100 btn btn-lg btn-dark" type="submit">Sign in</button>
-            </form>
-        </main>
-    </div>
-);
+class Login extends Component{
+    constructor() {
+      super();
+      // Stores user input (superrr secure)
+      this.state = {
+        email: '',
+        password: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+    handleSubmit(event) {
+      event.preventDefault();
+      
+      let success = this.checkInput()
+      if (success == -1)
+        console.log("Unsuccesful login");
+      else if (!(success % 2))
+        console.log("The user is a client: " + success);
+      else if (success % 2)
+        console.log("The user is an adm: " + success);
+      // window.location.href = "/";
+    }
+
+    checkInput(){
+        let clients = mockCreds.clients;
+        let adm = mockCreds.adm;
+        //Looping through clients
+        for (var i = 0; i < clients.length; i++) {
+          var obj = clients[i];
+          if (obj.email == this.state.email && obj.password == this.state.password)
+            return obj.id;
+        }
+        //Looping through adms
+        for (var i = 0; i < adm.length; i++) {
+          var obj = adm[i];
+          if (obj.email == this.state.email && obj.password == this.state.password)
+            return obj.id;
+        }
+        return -1;
+    }
+
+
+    render(){
+        return (
+            <div class="container py-4">
+                <main class="form-signin text-center">
+                    <form onSubmit={this.handleSubmit}>
+                        <h1 class="h3 mb-3 fw-normal">Login</h1>
+                        <div class="form-floating">
+                            <input name="email" type="email" class="form-control" id="Email" placeholder="name@example.com" onChange={this.handleChange} required/>
+                            <label for="floatingInput">Email address</label>
+                        </div>
+                        <div class="form-floating">
+                            <input name="password" type="password" class="form-control" id="Password" placeholder="Password"  onChange={this.handleChange} required/>
+                            <label for="floatingPassword">Passwords</label>
+                        </div>
+                        <div class="mb-3">
+                            <a href="/signup">Not registered yet? Sign up!</a>
+                        </div>
+                        <button class="w-100 btn btn-lg btn-dark" type="submit">Sign in</button>
+                    </form>
+                </main>
+            </div>
+        );
+    }
+}
 
 export default Login;
