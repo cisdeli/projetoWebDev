7b0b5fc512bc3a11d2a1301c2352ecee142d7191
Pedro Cisdeli <pedrohcsideli@usp.br>
2021-06-26 01:46:44 -0300

    Removed useless buttons

--- frontend/src/components/cartCheckout/cart.js
+++ frontend/src/components/cartCheckout/cart.js
@@ -1,8 +1,6 @@
 import React from "react";
 import '../../css/cart.css';
 
-
-var hasItem = 0;
 function Cart(){
     function addButton(){
         return(
@@ -32,15 +30,13 @@ function Cart(){
         return(
             <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded">
                 <div class="mr-1"><img class="rounded" src="https://i.imgur.com/XiFJkhI.jpg" width="70"/></div>
-                <div class="d-flex flex-column align-items-center product-details"><span class="font-weight-bold">Template</span>
+                <div class="d-flex flex-column align-items-center product-details"><span class="font-weight-bold">Name of the product</span>
                     <div class="d-flex flex-row product-desc">
-                        <div class="size mr-1"><span class="text-grey">Description</span></div>
+                        <div class="size mr-1"><span class="text-grey">Description of the product</span></div>
                     </div>
                 </div>
                 <div class="d-flex flex-row align-items-center qty">
-                    <button id="btn"><i class="fa fa-minus text-danger"></i></button>
-                    <h5 class="text-grey mt-1 mr-1 ml-1">2</h5>
-                    <button id="btn"><i class="fa fa-plus text-success"></i></button>
+                    <h5 class="text-grey mt-1 mr-1 ml-1">1</h5>
                 </div>
                 <div>
                     <h5 class="text-primary">$1234.56</h5>
