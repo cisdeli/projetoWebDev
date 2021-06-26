34b346d9e82619058883085a5974d24deb21f084
Pedro Cisdeli <pedrohcsideli@usp.br>
2021-06-26 01:54:19 -0300

    Better grid component

--- frontend/src/components/products/grid.js
+++ frontend/src/components/products/grid.js
@@ -1,185 +1,46 @@
 import React from "react";
 
-const Grid = () => (
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
+function Grid(){
+    function itemPreview(){
+        return(
+            <div class="col">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                    </svg>
 
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
+                    <div class="card-body">
+                        <p class="card-text">Product description</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                        </svg>
-
-                        <div class="card-body">
-                            <p class="card-text">Product description</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                    <a href="/itemPage"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Add to cart</button>
-                                </div>
-                                <h5><small class="text-primary">R$1234.56</small></h5>
-                            </div>
+                            <h5><small class="text-primary">R$1234.56</small></h5>
                         </div>
                     </div>
                 </div>
             </div>
+        );
+    }
+
+    function formGrid(){
+        let grid = [], length = 9;
+        for(let i = 0; i < length; i++)
+            grid.push(itemPreview());
+        return grid;
+    }
+
+    return(
+        <div class="album py-5 bg-light">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    {formGrid()}
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Grid;
