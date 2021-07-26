583618af38cf2dc401302d740d40721a932cb02b
sprmbng <pedrohcsideli@usp.br>
2021-07-26 14:37:33 -0300

    Removed old test file

--- server/src/Test.js
+++
@@ -1,9 +0,0 @@
-require('./app.js'); // which executes 'mongoose.connect()'
-
-var mongoose = require('mongoose');
-console.log(mongoose.connection.readyState);
-
-
-// https://medium.com/swlh/how-to-create-your-first-mern-mongodb-express-js-react-js-and-node-js-stack-7e8b20463e66
-// https://www.youtube.com/watch?v=_02zK1D4brk&ab_channel=CodingTheSmartWay.com
-// C:\Users\Pedro\Documents\Coding\projetoWebDev\server\src>
